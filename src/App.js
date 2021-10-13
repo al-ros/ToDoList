2d90e9cf0a76032f5ce34d6fc12754ed7e18af0a
Al <Ros>
2021-10-13 12:53:06 +0300

    In functions changed variable "inputTargetValue" to "currentTask" because self misunderstanding 'Date.now()' or 'new Date().getTime()' - What is the difference

--- src/App.js
+++ src/App.js
@@ -8,34 +8,45 @@ function App() {
   
   const [tasks, setTasks] = useState([])
   const [inputTargetValue, setInputTargetValue] = useState('')
+  const [taskEditing, setTaskEditing] = useState(null)
+  const [editingText, setEditingText] = useState('')
 
   const handleInputTargetSubmit = () => {
-      setTasks([...tasks, {
+      setTasks([...tasks, 
+        {
           task: inputTargetValue,
-          id: new Date().getTime(),
+          id: Date.now(), // or new Date().getTime() V CHEM RAZNIZA?!
           completed: false
       }])
       setInputTargetValue('')
   }
 
   const taskDone = (id) => {
-    const index = tasks.findIndex(x => x.id === id);
+  const index = tasks.findIndex(x => x.id === id);
     tasks.push(tasks.splice(index, 1)[0]);
-    const updatedTasks = [...tasks].map((inputTargetValue) => {
-      if (inputTargetValue.id === id) {
-        inputTargetValue.completed = !inputTargetValue.completed
-      } 
-      return inputTargetValue
+    const updatedTasks = [...tasks].map((currentTask) => {
+      if (currentTask.id === id) {
+        currentTask.completed = !currentTask.completed
+    }
+      return currentTask
     })
     setTasks(updatedTasks)
   }
   
   const editTask = (id) => {
- 
+    const updatedTasks = [...tasks].map((currentTask) => {
+      if (currentTask.id === id) {
+        currentTask.text = editingText
+    }
+      return currentTask
+    })
+    setTasks(updatedTasks)
+    setTaskEditing(null)
+    setEditingText('')
   }
 
   const deleteTask = (id) => {
-    const updatedTasks = [...tasks].filter((inputTargetValue) => inputTargetValue.id !== id )
+    const updatedTasks = [...tasks].filter((currentTask) => currentTask.id !== id )
     setTasks(updatedTasks)
     // console.log(updatedTasks)
   }
@@ -51,6 +62,8 @@ function App() {
       <main className="App__main">
         {tasks.map(({task, id, completed}) => <Task key={id} task={task} id={id} completed={completed} taskDone={taskDone} editTask={editTask} deleteTask={deleteTask}/>)}
       </main>
+
+      <h1>{console.log(tasks)}</h1>
 
       <footer className="App__footer">
       <InputTarget onChange={setInputTargetValue} onSubmit={handleInputTargetSubmit} value={inputTargetValue} />
